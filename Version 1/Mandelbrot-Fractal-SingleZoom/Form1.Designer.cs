a9bfadcd0d33733489deb47360938584a0109834
Madome31 <little-genius@online.de>
2016-05-13 11:16:57 +0200

    The first Version

---
+++ Version 1/Mandelbrot-Fractal-SingleZoom/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Mandelbrot_Fractal_SingleZoom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fractalBox = new System.Windows.Forms.PictureBox();
+            this.calculateStartImage = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resulutionBox = new System.Windows.Forms.ComboBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.datatypeBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.fractalBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fractalBox
+            // 
+            this.fractalBox.Location = new System.Drawing.Point(12, 12);
+            this.fractalBox.Name = "fractalBox";
+            this.fractalBox.Size = new System.Drawing.Size(884, 600);
+            this.fractalBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.fractalBox.TabIndex = 0;
+            this.fractalBox.TabStop = false;
+            // 
+            // calculateStartImage
+            // 
+            this.calculateStartImage.Location = new System.Drawing.Point(902, 12);
+            this.calculateStartImage.Name = "calculateStartImage";
+            this.calculateStartImage.Size = new System.Drawing.Size(153, 23);
+            this.calculateStartImage.TabIndex = 1;
+            this.calculateStartImage.Text = "Startbild";
+            this.calculateStartImage.UseVisualStyleBackColor = true;
+            this.calculateStartImage.Click += new System.EventHandler(this.calculateStartImage_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(902, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Auflösung";
+            // 
+            // resulutionBox
+            // 
+            this.resulutionBox.FormattingEnabled = true;
+            this.resulutionBox.Items.AddRange(new object[] {
+            "HD",
+            "FullHD",
+            "4k",
+            "8k",
+            "16k"});
+            this.resulutionBox.Location = new System.Drawing.Point(902, 83);
+            this.resulutionBox.Name = "resulutionBox";
+            this.resulutionBox.Size = new System.Drawing.Size(121, 21);
+            this.resulutionBox.TabIndex = 5;
+            this.resulutionBox.Text = "FullHD";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(905, 589);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(150, 23);
+            this.saveButton.TabIndex = 6;
+            this.saveButton.Text = "Speichern";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(902, 107);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(29, 13);
+            this.timeLabel.TabIndex = 7;
+            this.timeLabel.Text = "0 ms";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(899, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Datatype";
+            // 
+            // datatypeBox
+            // 
+            this.datatypeBox.FormattingEnabled = true;
+            this.datatypeBox.Items.AddRange(new object[] {
+            "Float",
+            "Double",
+            "Decimal",
+            "SqlDecimal"});
+            this.datatypeBox.Location = new System.Drawing.Point(902, 136);
+            this.datatypeBox.Name = "datatypeBox";
+            this.datatypeBox.Size = new System.Drawing.Size(121, 21);
+            this.datatypeBox.TabIndex = 9;
+            this.datatypeBox.Text = "Double";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1067, 624);
+            this.Controls.Add(this.datatypeBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.resulutionBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calculateStartImage);
+            this.Controls.Add(this.fractalBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.fractalBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox fractalBox;
+        private System.Windows.Forms.Button calculateStartImage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox resulutionBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox datatypeBox;
+    }
+}
+
---
+++ Version 1/Mandelbrot-Fractal-SingleZoom/Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace Mandelbrot_Fractal_SingleZoom
+{
+    public partial class Form1 : Form
+    {
+        private int _iterations = 1000;
+        private ushort _colorRed = 2, _colorGreen = 3, _colorBlue = 195;
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.DefaultExt = ".png";
+
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                fractalBox.Image.Save(sfd.FileName);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateStartImage_Click(object sender, EventArgs e)
+        {
+            Bitmap bit = generateRawImage();
+            fractalBox.Image = drawStartImage(bit);
+            fractalBox.Refresh();
+
+        }
+
+        private Bitmap drawStartImage(Bitmap b)
+        {
+
+            Stopwatch w = new Stopwatch();
+            w.Start();
+
+            if (datatypeBox.Text == "Float")
+            {
+                #region float
+                float x, y, x1, y1, xx, xmin, xmax, ymin, ymax = (float)0.0;
+
+                int looper, s, z = 0;
+                float intigralX, intigralY = (float)0.0;
+
+                Rectangle rect = new Rectangle(0, 0, b.Width, b.Height);
+                var data = b.LockBits(rect, ImageLockMode.WriteOnly, b.PixelFormat);
+
+                byte[] buffer = new byte[data.Height * data.Width * 3];//3, because of three colors per pixel
+
+                xmin = (float)-2.1; // Start x value, normally -2.1
+                ymin = (float)-1.3; // Start y value, normally -1.3
+                xmax = 1; // Finish x value, normally 1
+                ymax = (float)1.3; // Finish y value, normally 1.3
+                intigralX = (xmax - xmin) / b.Width; // Make it fill the whole window
+                intigralY = (ymax - ymin) / b.Height;
+                x = xmin;
+
+                for (s = 1; s < data.Width; s++)
+                {
+                    y = ymin;
+                    for (z = 1; z < data.Height; z++)
+                    {
+                        x1 = 0;
+                        y1 = 0;
+                        looper = 0;
+                        while (looper < _iterations && (x1 * x1) + (y1 * y1) < 4)
+                        {
+                            looper++;
+                            xx = (x1 * x1) - (y1 * y1) + x;
+                            y1 = 2 * x1 * y1 + y;
+                            x1 = xx;
+                        }
+
+                        // Get the percent of where the looper stopped
+                        double perc = looper / ((double)_iterations);
+                        // Get that part of a 255 scale
+                        int val = ((int)(perc * 255));
+
+                        // Use that number to set the color
+                        int offset = ((z * data.Height) + s) * 3;
+
+                        buffer[offset + 0] = Convert.ToByte(val * _colorBlue % 255);
+                        buffer[offset + 1] = Convert.ToByte(val * _colorGreen % 255);
+                        buffer[offset + 2] = Convert.ToByte(val * _colorRed % 255);
+                        y += intigralY;
+                    }
+                    x += intigralX;
+                }
+                Marshal.Copy(buffer, 0, data.Scan0, buffer.Length);
+                b.UnlockBits(data);
+
+                #endregion
+            }
+
+            else if (datatypeBox.Text == "Double")
+            {
+                #region double
+
+                double x, y, x1, y1, xx, xmin, xmax, ymin, ymax = 0.0;
+
+                int looper, s, z = 0;
+                double intigralX, intigralY = 0.0;
+
+                Rectangle rect = new Rectangle(0, 0, b.Width, b.Height);
+                var data = b.LockBits(rect, ImageLockMode.WriteOnly, b.PixelFormat);
+
+                byte[] buffer = new byte[data.Height * data.Width * 3];//3, because of three colors per pixel
+
+                xmin = -2.1; // Start x value, normally -2.1
+                ymin = -1.3; // Start y value, normally -1.3
+                xmax = 1; // Finish x value, normally 1
+                ymax = 1.3; // Finish y value, normally 1.3
+                intigralX = (xmax - xmin) / b.Width; // Make it fill the whole window
+                intigralY = (ymax - ymin) / b.Height;
+                x = xmin;
+
+                for (s = 1; s < data.Width; s++)
+                {
+                    y = ymin;
+                    for (z = 1; z < data.Height; z++)
+                    {
+                        x1 = 0;
+                        y1 = 0;
+                        looper = 0;
+                        while (looper < _iterations && (x1 * x1) + (y1 * y1) < 4)
+                        {
+                            looper++;
+                            xx = (x1 * x1) - (y1 * y1) + x;
+                            y1 = 2 * x1 * y1 + y;
+                            x1 = xx;
+                        }
+
+                        // Get the percent of where the looper stopped
+                        double perc = looper / ((double)_iterations);
+                        // Get that part of a 255 scale
+                        int val = ((int)(perc * 255));
+
+                        // Use that number to set the color
+                        int offset = ((z * data.Height) + s) * 3;
+
+                        buffer[offset + 0] = Convert.ToByte(val * _colorBlue % 255);
+                        buffer[offset + 1] = Convert.ToByte(val * _colorGreen % 255);
+                        buffer[offset + 2] = Convert.ToByte(val * _colorRed % 255);
+                        y += intigralY;
+                    }
+                    x += intigralX;
+                }
+                Marshal.Copy(buffer, 0, data.Scan0, buffer.Length);
+                b.UnlockBits(data);
+                #endregion
+            }
+            else if (datatypeBox.Text == "Decimal")
+            {
+                #region decimal
+                decimal x, y, x1, y1, xx, xmin, xmax, ymin, ymax = (decimal)0.0;
+
+                int looper, s, z = 0;
+                decimal intigralX, intigralY = (decimal)0.0;
+
+                Rectangle rect = new Rectangle(0, 0, b.Width, b.Height);
+                var data = b.LockBits(rect, ImageLockMode.WriteOnly, b.PixelFormat);
+
+                byte[] buffer = new byte[data.Height * data.Width * 3];//3, because of three colors per pixel
+
+                xmin = (decimal)-2.1; // Start x value, normally -2.1
+                ymin = (decimal)-1.3; // Start y value, normally -1.3
+                xmax = 1; // Finish x value, normally 1
+                ymax = (decimal)1.3; // Finish y value, normally 1.3
+                intigralX = (xmax - xmin) / b.Width; // Make it fill the whole window
+                intigralY = (ymax - ymin) / b.Height;
+                x = xmin;
+
+                for (s = 1; s < data.Width; s++)
+                {
+                    y = ymin;
+                    for (z = 1; z < data.Height; z++)
+                    {
+                        x1 = 0;
+                        y1 = 0;
+                        looper = 0;
+                        while (looper < _iterations && (x1 * x1) + (y1 * y1) < 4)
+                        {
+                            looper++;
+                            xx = (x1 * x1) - (y1 * y1) + x;
+                            y1 = 2 * x1 * y1 + y;
+                            x1 = xx;
+                        }
+
+                        // Get the percent of where the looper stopped
+                        double perc = looper / ((double)_iterations);
+                        // Get that part of a 255 scale
+                        int val = ((int)(perc * 255));
+
+                        // Use that number to set the color
+                        int offset = ((z * data.Height) + s) * 3;
+
+                        buffer[offset + 0] = Convert.ToByte(val * _colorBlue % 255);
+                        buffer[offset + 1] = Convert.ToByte(val * _colorGreen % 255);
+                        buffer[offset + 2] = Convert.ToByte(val * _colorRed % 255);
+                        y += intigralY;
+                    }
+                    x += intigralX;
+                }
+                Marshal.Copy(buffer, 0, data.Scan0, buffer.Length);
+                b.UnlockBits(data);
+                #endregion
+            }
+            else//Really slow, don´t even think about using it.
+            {
+                #region SqlDecimal
+
+                SqlDecimal x, y, x1, y1, xx, xmin, xmax, ymin, ymax = (SqlDecimal)0.0;
+
+                int looper, s, z = 0;
+                SqlDecimal intigralX, intigralY = (SqlDecimal)0.0;
+
+                Rectangle rect = new Rectangle(0, 0, b.Width, b.Height);
+                var data = b.LockBits(rect, ImageLockMode.WriteOnly, b.PixelFormat);
+
+                byte[] buffer = new byte[data.Height * data.Width * 3];//3, because of three colors per pixel
+
+                xmin = (decimal)-2.1; // Start x value, normally -2.1
+                ymin = (decimal)-1.3; // Start y value, normally -1.3
+                xmax = 1; // Finish x value, normally 1
+                ymax = (SqlDecimal)1.3; // Finish y value, normally 1.3
+                intigralX = (xmax - xmin) / b.Width; // Make it fill the whole window
+                intigralY = (ymax - ymin) / b.Height;
+                x = xmin;
+
+                for (s = 1; s < data.Width; s++)
+                {
+                    y = ymin;
+                    for (z = 1; z < data.Height; z++)
+                    {
+                        x1 = 0;
+                        y1 = 0;
+                        looper = 0;
+                        while (looper < _iterations && (x1 * x1) + (y1 * y1) < 4)
+                        {
+                            looper++;
+                            xx = (x1 * x1) - (y1 * y1) + x;
+                            y1 = 2 * x1 * y1 + y;
+                            x1 = xx;
+                        }
+
+                        // Get the percent of where the looper stopped
+                        double perc = looper / ((double)_iterations);
+                        // Get that part of a 255 scale
+                        int val = ((int)(perc * 255));
+
+                        // Use that number to set the color
+                        int offset = ((z * data.Height) + s) * 3;
+
+                        buffer[offset + 0] = Convert.ToByte(val * _colorBlue % 255);
+                        buffer[offset + 1] = Convert.ToByte(val * _colorGreen % 255);
+                        buffer[offset + 2] = Convert.ToByte(val * _colorRed % 255);
+                        y += intigralY;
+                    }
+                    x += intigralX;
+                }
+                Marshal.Copy(buffer, 0, data.Scan0, buffer.Length);
+                b.UnlockBits(data);
+
+                #endregion 
+            }
+
+            w.Stop();
+            timeLabel.Text = w.ElapsedMilliseconds.ToString() + " ms";
+
+            return b;
+        }
+        //These are squares!!!
+        private Bitmap generateRawImage()
+        {
+            Bitmap b = null;
+
+            if (resulutionBox.Text == "HD")
+            {
+                b = new Bitmap(1280, 1280, PixelFormat.Format24bppRgb);
+            }
+            else if (resulutionBox.Text == "FullHD")
+            {
+                b = new Bitmap(1920, 1920, PixelFormat.Format24bppRgb);
+            }
+            else if (resulutionBox.Text == "4k")
+            {
+                b = new Bitmap(3840, 3840, PixelFormat.Format24bppRgb);
+            }
+            else if (resulutionBox.Text == "8k")
+            {
+                b = new Bitmap(7680, 7680, PixelFormat.Format24bppRgb);
+            }
+            else if (resulutionBox.Text == "16k")
+            {
+                b = new Bitmap(15360, 15360, PixelFormat.Format24bppRgb);
+            }
+            return b;
+        }
+    }
+}
